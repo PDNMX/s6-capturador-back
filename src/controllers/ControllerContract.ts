7300e737a170bf7a9336e67467541274b32c03ee
Héctor Franco <hhernandezf1800@alumno.ipn.mx>
2024-07-10 10:41:27 -0600

    agregando ruta para probar insertt y primera version del esquema de yup, donde se valida lo que se envia desde el front del angular

--- src/controllers/ControllerContract.ts
+++ src/controllers/ControllerContract.ts
@@ -2,7 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import { querySchema } from '../schemas/yup.query';
 import { ClientError } from '../exceptions/clientError';
 import { ContractModel } from '../models/contracts.model';
-
+//import * as yup from 'yup';
+import { contractData } from '../schemas/contract.yup.query';
 class ControllerContracts {
   static checkRequest = async (req: Request, res: Response, next: NextFunction) => {
     const { body } = req;
@@ -27,9 +28,28 @@ class ControllerContracts {
   };
 
   static insertContract = async (req: Request, res: Response, next: NextFunction) => {
-    const data = await ContractModel.insertContract(req);
-    res.json(data);
+    
+ 
+/*     const contractValueData = {
+      value: {
+        amount: 1000+1,
+        currency: ['USD'],
+      },
+    }; */
+    const contractValueData = req.body;
+    try {
+      await contractData.validate(contractValueData);
+      res.send({messg:"todo muy bien"});
+    } catch (err: any) {
+      throw new ClientError('Contracts_8002', 'Error en la consulta enviada', `${err.path}: ${err.errors}`);
+      res.send({"status": "chale"});
+    }
+
+    /* const data = await ContractModel.insertContract(req);
+    res.json(data); */
   }
+
+  
 }
 
 export default ControllerContracts;
--- src/schemas/contract.yup.query.ts
+++ src/schemas/contract.yup.query.ts
@@ -1,11 +1,97 @@
-import { object, string, number, date, InferType } from 'yup';
+//import * as yup from 'yup';
+import { object, number, string, InferType, array } from "yup";
+export const contractData = object({
+    id: number().required("El id es obligatorio"),
+    awardID: string().required("El id de la adjudicación es obligatorio"),
+    title: string(),
+    description: string(),
+    status: string().required("El status es obligatorio"),
+    period: object({
+      startDate: string(), // .required('La fecha de inicio es obligatoria'),
+      endDate: string(),
+      durationInDays: number(),
+      maxExtentDate: string(),
+    value: object({
+      amount: number(),
+      amountNet: number(),
+      currency: string(),
+    }),
+    surveillanceMechanisms: array().of(string()),
+    dateSignedContracts: object({
+      dateSigned: string(),
+    }),
+  }),
+});
 
-export const querySchema = object({
+export const itemContractData = object({
+  id: number().required("El id es obligatorio"),
+  description: string(),
+  classification: object({
+    scheme: string(),
+    id: string(),
+    description: string(),
+    uri: string(),
+  }),
+  additionalClassifications: array().of(
+    object({
+      scheme: string(),
+      id: string(),
+      description: string(),
+      uri: string(),
+    })
+  ),
+  quantity: number(),
+  unit: object({
+    scheme: string(),
+    id: string(),
+    name: string(),
+    uri: string(),
+    value: object({
+      amount: number(),
+      currency: string(),
+    }),
+  }),
+  deliveryLocation: object({
+    uri: string(),
+    description: string(),
+    geometry: object({
+      type: string(),
+      coordinates: array().of(
+        object({
+          latitude: string(),
+          longitude: string(),
+        })
+      ),
+      gazetteer: object({
+        scheme: string(),
+        idenfiers: string(),
+      }),
+    }),
+  }),
+  deliveryAddress: object({
+    uri: string(),
+    description: string(),
+    streetAddress: string(),
+    locality: string(),
+    region: string(),
+    postalCode: string(),
+    countryName: string(),
+  }),
+});
+
+/* const contractValueSchema = yup.object().shape({
+    value: yup.object().shape({
+      amount: yup.number().required('El monto es obligatorio'),
+      currency: yup.string().required('La moneda es obligatoria'),
+    }),
+} );*/
+
+/* export const querySchema = object({
     page: number().integer('solo acepta numeros enteros.').positive('solo acepta valores positivos.'),
     pageSize: number().integer(),
-/*     sort: object({
+    sort: object({
         
-    }), */
+    }),
     query: object({
         
     })
@@ -13,3 +99,4 @@ export const querySchema = object({
 
 export type IQuery = InferType<typeof querySchema>;
 
+ */
