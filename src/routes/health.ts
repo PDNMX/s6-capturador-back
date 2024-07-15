16098c58e26b771dc51adb80e3ba4d3695ab5c38
Benito Perdomo <benito.perdomo.v@gmail.com>
2024-07-15 00:01:39 -0600

    new routes record

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -8,5 +8,7 @@ const router = Router();
 const validations = [checkJwt, asyncHandler(ControllerHealth.checkRequest)];
 
 router.post('/', validations, asyncHandler(ControllerHealth.executeQuery));
+router.post('/getAll', validations, asyncHandler(ControllerHealth.getAll));
+router.post('/query', validations, asyncHandler(ControllerHealth.query));
 
 export default router;
