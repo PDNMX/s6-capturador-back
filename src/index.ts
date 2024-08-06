4ddcd1febf3e03d06d503867a316d20b5c58e651
Héctor Franco <hhernandezf1800@alumno.ipn.mx>
2024-08-06 13:32:00 -0600

    modifing methods restful in back

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,9 @@ const app = express();
 app.use(json());
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*'); // Considera restringir esto a tu dominio específico en producción
     res.header('Access-Control-Allow-Credentials', 'true');
-    // res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS, DELETE');
     res.header('Access-Control-Expose-Headers', 'Content-Length');
     res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
     if (req.method === 'OPTIONS') {
@@ -21,10 +20,9 @@ app.use(function (req, res, next) {
     }
 });
 
-
 app.use('/' + config.prefix, routes);
 app.use(errorHandler);
 
 app.listen(config.port, () => {
-    console.log(`Servidor ejecutandose en el puerto: ${config.port}`);
+    console.log(`Servidor ejecutándose en el puerto: ${config.port}`);
 });
