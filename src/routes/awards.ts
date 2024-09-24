cd9be4e00e0eee5951042ad53e85a978ec219d34
Andrés López <alopezr2129@gmail.com>
2024-09-23 21:08:10 -0600

    endpoints configurations for awards form

--- src/routes/awards.ts
+++ src/routes/awards.ts
@@ -13,6 +13,6 @@ router.post('/getAll', validations, asyncHandler(ControllerAwards.getAll));
 //router.post("/update", /* validations, */ asyncHandler(ControllerAwards.executeQuery));
 router.post("/query", /* validations, */ asyncHandler(ControllerAwards.query));
 router.post("/getById", /* validations, */ asyncHandler(ControllerAwards.getById));
-router.put('/update', /* validations, */ asyncHandler(ControllerAwards.updateData));
+router.put('/update', validations, asyncHandler(ControllerAwards.updateData));
 
 export default router;
