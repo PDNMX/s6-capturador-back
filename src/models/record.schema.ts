fc58183e6fc77e2ce43f99432a0faa230506a370
Omar Franco <hhernandezf1800@alumno.ipn.mx>
2024-09-27 14:07:48 -0600

    merge with dev

--- src/models/record.schema.ts
+++ src/models/record.schema.ts
@@ -7,111 +7,10 @@ import { MetadataSchema } from './metadata/metadata.schema';
 import { AwardSchema } from './awards/award.schema';
 import { TenderSchema } from './tenders/tender.schema';
 import config from '../config';
+import { implementationSchema } from './implements/implementation.schema';
 
 let mongoo = mongooseService.getMongoose();
 let ocid = config.ocid;
-
-/* const ContractGeneralSchema: Schema = new mongoo.Schema({
-  id: String,
-  status: String,
-  awardID: String,
-  title: String,
-  description: String,
-  surveillanceMechanisms: String,
-  period: {
-    startDate: String,
-    endDate: String,
-    durationInDays: String,
-    maxExtentDate: String
-  },
-  value: {
-    amount: String,
-    amountNet: String,
-    currency: String
-  },
-  dateSignedContracts: {
-    dateSigned: String
-  },
-  items: {
-    id: String,
-    description: String,
-    clasification: {
-      scheme: String,
-      id: String,
-      uri: String,
-      description: String
-    },
-    additionalClassifications: {
-      scheme: String,
-      id: String,
-      uri: String,
-      description: String
-    },
-    quantity: String,
-    unit: {
-      scheme: String,
-      id: String,
-      name: String,
-      uri: String,
-      value: {
-        amount: String,
-        currency: String
-      }
-    },
-    deliveryLocation: {
-      uri: String,
-      description: String,
-      geometry: {
-        type: String,
-        coordinates: {
-          type: String,
-          coordinates: [String]
-        }
-      }
-    }
-  },
-  documents: {
-    id: String,
-    documentType: String,
-    title: String,
-    description: String,
-    url: String,
-    datePublished: String
-  },
-  guarantees: {
-    id: String,
-    description: String,
-    amount: String,
-    currency: String,
-    dateFrom: String,
-    dateTo: String
-  },
-  relatedProcesses: {
-    id: String,
-    relationship: String,
-    title: String
-  },
-  amendments: {
-    id: String,
-    date: String,
-    rationale: String,
-    amendsReleaseID: String,
-    releaseID: String
-  },
-  milestones: {
-    id: String,
-    title: String,
-    type: String,
-    description: String,
-    code: String,
-    dueDate: String,
-    dateMet: String,
-    dateModified: String,
-    status: String
-  }
-
-}); */
-
 
 const RecordSchema: Schema<IRecord> = new mongoo.Schema({
   id: String,
@@ -123,7 +22,8 @@ const RecordSchema: Schema<IRecord> = new mongoo.Schema({
   metadata: MetadataSchema,
   //contract: ContractSchema,
   award: AwardSchema,
-  tender: TenderSchema
+  tender: TenderSchema,
+  //implements: implementationSchema,
 });
 
 RecordSchema.set('toJSON', {
