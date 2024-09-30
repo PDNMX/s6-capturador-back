dac2ec80da1efee6ca84203d399ee1c502e954e4
Omar Franco <hhernandezf1800@alumno.ipn.mx>
2024-09-30 13:15:08 -0600

    contract scheme type modified properties item and guarantee

--- src/models/contracts/contract.schema.ts
+++ src/models/contracts/contract.schema.ts
@@ -4,7 +4,8 @@ import mongooseService from "../../common/services/mongoose.service";
 //import { IContract } from "./contract.interface";
 
 const mongoo = mongooseService.getMongoose();
-export const ContractSchema : Schema = new mongoo.Schema({
+export const ContractSchema: Schema = new mongoo.Schema(
+  {
     _id: String,
     id: Schema.Types.Mixed,
     status: Schema.Types.Mixed,
@@ -16,87 +17,91 @@ export const ContractSchema : Schema = new mongoo.Schema({
       startDate: Schema.Types.Mixed,
       endDate: Schema.Types.Mixed,
       durationInDays: Schema.Types.Mixed,
-      maxExtentDate: Schema.Types.Mixed
+      maxExtentDate: Schema.Types.Mixed,
     },
     value: {
       amount: Schema.Types.Mixed,
       amountNet: Schema.Types.Mixed,
-      currency: Schema.Types.Mixed
+      currency: Schema.Types.Mixed,
     },
     dateSignedContracts: {
-      dateSigned: Schema.Types.Mixed
+      dateSigned: Schema.Types.Mixed,
     },
-    items: {
-      id: Schema.Types.Mixed,
-      description: Schema.Types.Mixed,
-      clasification: {
-        scheme: Schema.Types.Mixed,
+    items: [
+      {
         id: Schema.Types.Mixed,
-        uri: Schema.Types.Mixed,
-        description: Schema.Types.Mixed
+        description: Schema.Types.Mixed,
+        clasification: {
+          scheme: Schema.Types.Mixed,
+          id: Schema.Types.Mixed,
+          uri: Schema.Types.Mixed,
+          description: Schema.Types.Mixed,
+        },
+        additionalClassifications: {
+          scheme: Schema.Types.Mixed,
+          id: Schema.Types.Mixed,
+          uri: Schema.Types.Mixed,
+          description: Schema.Types.Mixed,
+        },
+        quantity: Schema.Types.Mixed,
+        unit: {
+          scheme: Schema.Types.Mixed,
+          id: Schema.Types.Mixed,
+          name: Schema.Types.Mixed,
+          uri: Schema.Types.Mixed,
+          value: {
+            amount: Schema.Types.Mixed,
+            currency: Schema.Types.Mixed,
+          },
+        },
+        deliveryLocation: {
+          uri: Schema.Types.Mixed,
+          description: Schema.Types.Mixed,
+          geometry: {
+            type: Schema.Types.Mixed,
+            coordinates: {
+              latitude: Schema.Types.Mixed,
+              longitude: Schema.Types.Mixed,
+            },
+            gazetteer: {
+              scheme: Schema.Types.Mixed,
+              identifiers: Schema.Types.Mixed,
+            },
+          },
+        },
+        deliveryAddress: {
+          uri: Schema.Types.Mixed,
+          description: Schema.Types.Mixed,
+          streetAddress: Schema.Types.Mixed,
+          locality: Schema.Types.Mixed,
+          region: Schema.Types.Mixed,
+          postalCode: Schema.Types.Mixed,
+          countryName: Schema.Types.Mixed,
+        },
       },
-      additionalClassifications: {
-        scheme: Schema.Types.Mixed,
+    ],
+    guarantees: [
+      {
         id: Schema.Types.Mixed,
-        uri: Schema.Types.Mixed,
-        description: Schema.Types.Mixed
-      },
-      quantity: Schema.Types.Mixed,
-      unit: {
-        scheme: Schema.Types.Mixed,
-        id: Schema.Types.Mixed,
-        name: Schema.Types.Mixed,
-        uri: Schema.Types.Mixed,
+        type: Schema.Types.Mixed,
+        date: Schema.Types.Mixed,
+        obligations: Schema.Types.Mixed,
         value: {
           amount: Schema.Types.Mixed,
-          currency: Schema.Types.Mixed
-        }
+          currency: Schema.Types.Mixed,
+        },
+        guarantor: {
+          id: Schema.Types.Mixed,
+          name: Schema.Types.Mixed,
+        },
+        period: {
+          startDate: Schema.Types.Mixed,
+          endDate: Schema.Types.Mixed,
+          durationInDays: Schema.Types.Mixed,
+          maxExtentDate: Schema.Types.Mixed,
+        },
       },
-      deliveryLocation: {
-        uri: Schema.Types.Mixed,
-        description: Schema.Types.Mixed,
-        geometry: {
-          type: Schema.Types.Mixed,
-          coordinates: {
-            latitude: Schema.Types.Mixed,
-            longitude: Schema.Types.Mixed
-          },
-          gazetteer: {
-            scheme: Schema.Types.Mixed,
-            identifiers: Schema.Types.Mixed
-          }
-        }
-      },
-      deliveryAddress: {
-        uri: Schema.Types.Mixed,
-        description: Schema.Types.Mixed,
-        streetAddress: Schema.Types.Mixed,
-        locality: Schema.Types.Mixed,
-        region: Schema.Types.Mixed,
-        postalCode: Schema.Types.Mixed,
-        countryName: Schema.Types.Mixed
-      }
-    },
-    guarantees: {
-      id: Schema.Types.Mixed,
-      type: Schema.Types.Mixed,
-      date: Schema.Types.Mixed,
-      obligations: Schema.Types.Mixed,
-      value: {
-        amount: Schema.Types.Mixed,
-        currency: Schema.Types.Mixed
-      },
-      guarantor: {
-        id: Schema.Types.Mixed,
-        name: Schema.Types.Mixed
-      },
-      period: {
-        startDate: Schema.Types.Mixed,
-        endDate: Schema.Types.Mixed,
-        durationInDays: Schema.Types.Mixed,
-        maxExtentDate: Schema.Types.Mixed
-      }
-    },
+    ],
     documents: {
       id: Schema.Types.Mixed,
       documentType: Schema.Types.Mixed,
@@ -105,7 +110,7 @@ export const ContractSchema : Schema = new mongoo.Schema({
       uri: Schema.Types.Mixed,
       datePublished: Schema.Types.Mixed,
       dateModified: Schema.Types.Mixed,
-      format: Schema.Types.Mixed
+      format: Schema.Types.Mixed,
     },
     relatedProcesses: {
       id: Schema.Types.Mixed,
@@ -113,7 +118,7 @@ export const ContractSchema : Schema = new mongoo.Schema({
       title: Schema.Types.Mixed,
       scheme: Schema.Types.Mixed,
       identifier: Schema.Types.Mixed,
-      uri: Schema.Types.Mixed
+      uri: Schema.Types.Mixed,
     },
     milestones: {
       id: Schema.Types.Mixed,
@@ -124,7 +129,7 @@ export const ContractSchema : Schema = new mongoo.Schema({
       dueDate: Schema.Types.Mixed,
       dateMet: Schema.Types.Mixed,
       dateModified: Schema.Types.Mixed,
-      status: Schema.Types.Mixed
+      status: Schema.Types.Mixed,
     },
     amendments: {
       id: Schema.Types.Mixed,
@@ -132,6 +137,8 @@ export const ContractSchema : Schema = new mongoo.Schema({
       rationale: Schema.Types.Mixed,
       description: Schema.Types.Mixed,
       amendsReleaseID: Schema.Types.Mixed,
-      releaseID: Schema.Types.Mixed
-    }
-  }, { strict: false });
+      releaseID: Schema.Types.Mixed,
+    },
+  },
+  { strict: false }
+);
