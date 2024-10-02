19d16d8a817c59aef481276f065e2a5eba68e7dc
Benito Perdomo Vergara <benito.perdomo.v@gmail.com>
2024-10-02 15:17:17 -0600

    add getList of parties

--- src/routes/parties.ts
+++ src/routes/parties.ts
@@ -8,6 +8,8 @@ const router = Router();
 const validations = [checkJwt, asyncHandler(ControllerParties.checkRequest)];
 
 router.get('/:id', validations, asyncHandler(ControllerParties.getByID));
+router.get('/:id/list', validations, asyncHandler(ControllerParties.getList));
+router.get('/:id/list/:type', validations, asyncHandler(ControllerParties.getList));
 router.post('/:id', validations, asyncHandler(ControllerParties.addParties));
 
 export default router;
