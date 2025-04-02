fd4f06e729a249fee0e6b97ec8b999c54c051acc
Andrés López <102785677+alopezramon@users.noreply.github.com>
2025-04-01 19:41:33 -0600

    Revert "Alopez"

--- src/index.ts
+++ src/index.ts
@@ -6,28 +6,24 @@ import routes from './routes/index';
 
 const app = express();
 app.use(json());
-app.use((req, res, next) => {
-  console.log('req: ', JSON.stringify(req.body), JSON.stringify(req.params));
-  next();
-});
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  // res.header('Access-Control-Allow-Methods', 'POST');
-  res.header('Access-Control-Expose-Headers', 'Content-Length');
-  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  } else {
-    return next();
-  }
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    // res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    } else {
+        return next();
+    }
 });
 
 app.use('/' + config.prefix, routes);
 app.use(errorHandler);
 
 app.listen(config.port, () => {
-  console.log(`Servidor ejecutándose en el puerto: ${config.port}`);
+    console.log(`Servidor ejecutándose en el puerto: ${config.port}`);
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,8 +6,6 @@ import records from './records';
 import tenders from './tenders';
 import parties from './parties';
 import planning from './planning';
-import usersRouter from './users';
-import institutionRouter from './institution';
 
 const routes = Router();
 routes.use('/health', health);
@@ -18,7 +16,5 @@ routes.use('/tender', tenders);
 routes.use('/parties', parties);
 routes.use('/implements', contracts);
 routes.use('/planning', planning);
-routes.use('/users', usersRouter);
-routes.use('/institution', institutionRouter);
 
 export default routes;
